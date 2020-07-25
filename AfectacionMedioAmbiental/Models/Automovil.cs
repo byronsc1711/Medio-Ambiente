0e82ced8436ac310ec291da0e24c2977f8dbe749
byronsc1711 <byrondavid19@hotmail.com>
2020-07-25 18:16:42 -0500

    tarea 3

---
+++ AfectacionMedioAmbiental/Models/Automovil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AfectacionMedioAmbiental.Models
+{
+    public class Automovil
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+    }
+}
---
+++ AfectacionMedioAmbiental/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace AfectacionMedioAmbiental.Models
+{
+    public class Persona
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage =@"Ingrese el apellido")]
+        [DisplayName("El Apellido")]
+        public string Apellido { get; set; }
+        public Celular Celular { get; set; }
+    }
+}
