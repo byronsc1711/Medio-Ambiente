29f0d203089e706b16b5176e9c3e78a25b3f5c47
byronsc1711 <byrondavid19@hotmail.com>
2020-07-28 20:52:16 -0500

    tarea 4

--- AfectacionMedioAmbiental/Models/Automovil.cs
+++ AfectacionMedioAmbiental/Models/Automovil.cs
@@ -10,5 +10,9 @@ namespace AfectacionMedioAmbiental.Models
         public int Id { get; set; }
         public string Marca { get; set; }
         public string Modelo { get; set; }
-    }
+            
+        }
+        
+        
 }
+     
---
+++ AfectacionMedioAmbiental/Models/Programa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AfectacionMedioAmbiental.Models
+{
+    public class Programa
+    {
+        static void Main(string[] args)
+        {
+            CSuma s = new CSuma();
+            s.calcular(4.5, 6.7);
+            s.mostrar();
+        }
+    }
+}
